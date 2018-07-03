78ef17852f1e41021071b0e402bea8574e08dbc7
Zexin Yan <yzx9610@outlook.com>
2018-07-03 22:27:47 +0800

    before change to new sort version

--- main.cpp
+++ main.cpp
@@ -2,17 +2,16 @@
 #include "include/Data.h"
 //#include "include/randomForest.h"
 
-
 int main() {
-    Data trainData(true, 50);
-    trainData.read("../data/debug.txt");
+    Data trainData(true, 1719692);
+    trainData.read("../data/train.txt");
 
-    DecisionTree tree("gini", -1, 100, 1, -1, "log2");
+    DecisionTree tree("gini", 1, 100, 1, -1, "log2");
 
     tree.fit(trainData);
 
-    vector<double> results(trainData.getSampleSize(), 0);
-    tree.predictProba(trainData, results);
-    writeDataToCSV(results, trainData, "../results/debug.csv");
+//    vector<double> results(trainData.getSampleSize(), 0);
+//    tree.predictProba(trainData, results);
+//    writeDataToCSV(results, trainData, "../results/train.csv");
     return 0;
 }
--- src/DecisionTree.cpp
+++ src/DecisionTree.cpp
@@ -108,9 +108,10 @@ void DecisionTree::chooseBestSplitFeatures(shared_ptr<Node> &node,
     int bestFeatureIndex = featuresVec[0];
     int samplesTrueNum = computeTure(samplesVec, Data);
     double minValue = 1000000000, bestThreshold = 0;
-    double threshold = 0, splitInfo = 0;
+    double threshold = 0;
     int sampleIndex;
     for (auto featureIndex : featuresVec) {
+        auto now = time(NULL);
         Data.sortByFeature(samplesVec, featureIndex);
         int leftSize = 0, rightSize = (int)samplesVec.size();
         int leftTrue = 0, rightTrue = samplesTrueNum;
@@ -131,19 +132,12 @@ void DecisionTree::chooseBestSplitFeatures(shared_ptr<Node> &node,
             if (index == samplesVec.size()) { continue; }
             double value = criterionFunc(leftTrue, leftSize, rightTrue, rightSize);
             if (value <= minValue) {
-                if (value == minValue) {
-                    int info = (leftSize / 100) * (rightSize / 100);
-                    if (info < splitInfo) {
-                        continue;
-                    } else {
-                        splitInfo = info;
-                    }
-                }
                 minValue = value;
                 bestThreshold = threshold;
                 bestFeatureIndex = featureIndex;
             }
         }
+        cout << (time(NULL) - now) << endl;
     }
     node->featureIndex = bestFeatureIndex;
     node->threshold = bestThreshold;
