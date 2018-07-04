1be44858d1fe5238a73c715969ad5c85db97a76c
Zexin Yan <yzx9610@outlook.com>
2018-07-04 08:39:52 +0800

    init experiment

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@ int main() {
     Data trainData(true, 1719692);
     trainData.read("../data/train.txt");
 
-    DecisionTree tree("gini", 10, 100, 1, -1, "log2");
+    for (int i = 0; i < 100000; i++) {
+        for (int j = 0; j < 200; j++) {
+            trainData.readFeature(i, j);
+        }
+    }
 
-    tree.fit(trainData);
-
-    vector<double> results(trainData.getSampleSize(), 0);
-    tree.predictProba(trainData, results);
-    writeDataToCSV(results, trainData, "../results/train.csv");
+//    DecisionTree tree("gini", 10, 100, 1, -1, "log2");
+//
+//    tree.fit(trainData);
+//
+//    vector<double> results(trainData.getSampleSize(), 0);
+//    tree.predictProba(trainData, results);
+//    writeDataToCSV(results, trainData, "../results/train.csv");
     return 0;
 }
