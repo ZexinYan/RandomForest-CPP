c1950224b4814509411ddabd89d7df83499f89cc
Zexin Yan <yzx9610@outlook.com>
2018-07-04 14:59:05 +0800

    finish designing decision tree

--- main.cpp
+++ main.cpp
@@ -6,19 +6,12 @@ int main() {
     Data trainData(true, 1719692);
     trainData.read("../data/train.txt");
 
-    for (int i = 0; i < 1000000; i++) {
-        for (int j = 0; j < 201; j++) {
-            trainData.readFeature(i, j);
-//            cout << i << " " << j << " " << trainData.readFeature(i, j) << endl;
-        }
-    }
+    DecisionTree tree("gini", 10, 100, 1, -1, "log2");
 
-//    DecisionTree tree("gini", 10, 100, 1, -1, "log2");
-//
-//    tree.fit(trainData);
-//
-//    vector<double> results(trainData.getSampleSize(), 0);
-//    tree.predictProba(trainData, results);
-//    writeDataToCSV(results, trainData, "../results/train.csv");
+    tree.fit(trainData);
+
+    vector<double> results(trainData.getSampleSize(), 0);
+    tree.predictProba(trainData, results);
+    writeDataToCSV(results, trainData, "../results/train1.csv");
     return 0;
 }
