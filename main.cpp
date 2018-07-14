cc41dc8335ad0b654b954ebdfdc6229a2c7e2729
Zexin Yan <yzx9610@outlook.com>
2018-07-15 00:02:44 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     Data trainData(true, 1719692);
     trainData.read("../data/train.txt");
-//
+
     RandomForest randomForest(100, "gini", "log2", -1, 150, 1, 1000000, 8);
 
     randomForest.fit(trainData);
